284639288f754732edfc18681d03a649bea64662
Chief <520@zut.edu.cn>
2026-04-09 19:46:27 +0800

    fix: 修复 setup.py 中文件打包路径问题

--- src/g1_yolo_nav_py/setup.py
+++ src/g1_yolo_nav_py/setup.py
@@ -10,8 +10,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch", ["launch/*.launch.py"]),
-        ("share/" + package_name + "/config", ["config/*.yaml"]),
+        ("share/" + package_name + "/launch", glob.glob("launch/*.launch.py")),
+        ("share/" + package_name + "/config", glob.glob("config/*.yaml")),
         ("share/" + package_name + "/models", glob.glob("*.pt")),
     ],
     install_requires=["setuptools"],
